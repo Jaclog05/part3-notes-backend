0bacd62e01bb72ef6674ba8875d91d8a8244ced4
Jaider <jaidercueto.1010@gmail.com>
2025-12-17 13:00:18 -0500

    Installing cors. Changing port for deployment

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 let notes = [
@@ -27,6 +28,7 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
 
@@ -88,7 +90,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
