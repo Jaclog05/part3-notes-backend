8c79898930a91f951f1ab4bdb69286bdcdc3489c
Jaider <jaidercueto.1010@gmail.com>
2025-12-27 14:00:58 -0500

    Deploying backend with db to production

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if(error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -48,7 +50,7 @@ app.get('/api/notes/:id', (request, response, next) => {
     .catch((error) => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
   if (!body.content) {
@@ -63,23 +65,19 @@ app.post('/api/notes', (request, response) => {
   note.save().then((savedNote) => {
     response.json(savedNote)
   })
+  .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
   const { content, important } = request.body
 
-  Note.findById(request.params.id)
-    .then((note) => {
-      if (!note) {
-        return response.status(404).end()
-      }
-
-      note.content = content
-      note.important = important
-
-      return note.save().then((updatedNote) => {
-        response.json(updatedNote)
-      })
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then((updatedNote) => {
+      response.json(updatedNote)
     })
     .catch((error) => next(error))
 })
